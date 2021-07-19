a952eabb025c3f230d7449834eabbb42ba8f1e83
Bastian951 <70776507+Bastian951@users.noreply.github.com>
2021-07-19 17:27:18 -0400

    Actualización All.php conectando con la API

--- CRUD-simulacion/usuario/CRUD/all.php
+++ CRUD-simulacion/usuario/CRUD/all.php
@@ -2,21 +2,29 @@
   include '../../db_config.php';
   $sql = "SELECT * FROM usuario";
   $result = pg_query_params($dbconn, $sql, array());
-  if (pg_num_rows($result) > 0)
-  {
-    while ($row = pg_fetch_assoc($result))
-    {
-      $direccion_read = '<a href="/admin/users/read.html?id=' . $row["id"] . '" class="btn btn-primary">Ver <i class="fas fa-search"></i></a>';
-      //<a href="/admin/users/read.html?id=$ontador" class="btn btn-primary">Ver <i class="fas fa-search"></i></a>
-      $direccion_edit = '<a href="/admin/users/update.html?id=' . $row["id"] . '" class="btn btn-warning">Editar <i class="fas fa-edit"></i></a>';
-      //'<a href="/admin/users/update.html?id=$contador" class="btn btn-warning">Editar <i class="fas fa-edit"></i></a>'
-      $direccion_delete = '<a href="/admin/users/CRUD/delete.php?id=' . $row["id"] . '" class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a>';
-      //'<a href="/admin/users/CRUD/delete.php?id=$contador class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a>'
-      echo '<tr>' . '<td>' . $row["id"] . '<td>' . $row["nombre"] . '<td>' . $row["apellido"] . '<td>' . $row["correo"] . '<td>' .
+
+
+$json = file_get_contents('http://127.0.0.1:5000/api/usuario');
+
+// Converts it into a PHP object
+$usuarios = json_decode($json,true)["usuarios"];
+
+foreach($usuarios as $i => $value){
+
+      $direccion_read = '<a href="/CRUD-simulacion/usuario/read.html?id=' . $value["id"] . '" class="btn btn-primary">Ver <i class="fas fa-search"></i></a>';
+  
+      $direccion_edit = '<a href="/CRUD-simulacion/usuario/update.html?id=' . $value["id"] . '" class="btn btn-warning">Editar <i class="fas fa-edit"></i></a>';
+  
+      $direccion_delete = '<a href="/CRUD-simulacion/usuario/CRUD/delete.php?id=' . $value["id"] . '" class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a>';
+   
+      echo '<tr>' . '<td>' . $value["id"] . '<td>' . $value["nombre"] . '<td>' . $value["apellido"] . '<td>' . $value["correo"] . '<td>' .
           $direccion_read . '<td>' .
           $direccion_edit . '<td>' .
-          $direccion_delete;
-    }
-  }
+          $direccion_delete;	
+
+
+}
+
+ 
 /* Este archivo debe manejar la lógica de obtener los datos de todos los usuarios */
 ?>
