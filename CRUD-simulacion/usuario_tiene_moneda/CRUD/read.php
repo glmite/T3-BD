98b909827a5acfbd796395d6f6467f17faacf966
Nilsson7000 <85040945+Nilsson7000@users.noreply.github.com>
2021-07-21 18:39:07 -0400

    Update read.php

--- CRUD-simulacion/usuario_tiene_moneda/CRUD/read.php
+++ CRUD-simulacion/usuario_tiene_moneda/CRUD/read.php
@@ -1,34 +1,56 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT']. '/sesion/valida_sesion.php';
 
-// es esta la manera más eficiente?
-// recordar id usuario
-
-$id = preg_replace('#/admin/users/read.html\?id=#', '', $_SERVER['REQUEST_URI']);
-
+error_reporting(0);
+$id = preg_replace('#/CRUD-simulacion/usuario_tiene_moneda/read.html\?id=#', '', $_SERVER['REQUEST_URI']);
+$id_sep = explode(',', $id);
 if($id != '') {
-$query = 
-' SELECT usuario.nombre AS usuario
-,apellido,correo,pais.nombre AS pais
-,fecha_registro,id FROM 
-pais JOIN usuario
- ON usuario.pais=pais.cod_pais
- WHERE id = $1
- ';
 
-$result = pg_query_params($dbconn, $query, array($id));
+$json_usuario_tiene_moneda = file_get_contents('http://127.0.0.1:5000/api/usuario_tiene_moneda');
+$json_usuario = file_get_contents('http://127.0.0.1:5000/api/usuario');
+$json_moneda = file_get_contents('http://127.0.0.1:5000/api/moneda');
 
-if( $result !== FALSE ) {
-    pg_close($dbconn);
-    $info_usuario = pg_fetch_assoc($result);
-} else {
-    echo "Usuario no existe";    
-    pg_close($dbconn);
-}
+$usuario_tiene_moneda = json_decode($json_usuario_tiene_moneda,true)["usuario moneda"];
+$usuario = json_decode($json_usuario,true)["usuarios"];
+$moneda = json_decode($json_moneda,true)["monedas"];
+
+//Filtramos el json usuario_tiene_moneda
+
+$usuario_tiene_moneda = array_filter($usuario_tiene_moneda, function ($var) use ($id_sep) {
+    return ($var['id_usuario,id_moneda'] == $id);
+});
+
+foreach($usuario_tiene_moneda as $i => $value){ $usuario_tiene_moneda = $value; }
+
+//Filtramos el json pais
+
+$usuario = array_filter($usuario, function ($var) use ($id_sep) {
+    return ($var['id'] == $id_sep[0]);
+});
+
+foreach($usuario as $i => $value){ $usuario = $value; }
+
+//Filtramos el json pais
+
+
+$moneda = array_filter($moneda, function ($var) use ($id_sep) {
+    return ($var['id'] == $id_sep[1]);
+});
+
+foreach($moneda as $i => $value){ $moneda = $value; }
+
+// Guardamos los valores
+$id_usuario = $usuario["id"];
+$nombre = $usuario["nombre"];
+$apellido = $usuario["apellido"];
+$id_moneda = $moneda["id"];
+$moneda = $moneda["nombre"];
+$balance = $usuario_tiene_moneda["balance"];
+
+
 }else{
-    pg_close($dbconn);   
+    pg_close($dbconn);
     header( "Location: /");
 }
-    
+
 
 ?>
