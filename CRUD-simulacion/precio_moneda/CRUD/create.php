7895550d0bdcd0c833e86a2df9a01faad34a8e8a
unknown <amanda.salinas@sansano.usm.cl>
2021-07-21 18:18:29 -0400

    Se agrega create.php

--- CRUD-simulacion/precio_moneda/CRUD/create.php
+++ CRUD-simulacion/precio_moneda/CRUD/create.php
@@ -1,36 +1,52 @@
-<?php 
+<?php
 include $_SERVER['DOCUMENT_ROOT']. '/include/header.html';
 include $_SERVER['DOCUMENT_ROOT']. '/sesion/valida_sesion.php';
 
+
+function callAPI($method, $url, $data){
+    $curl = curl_init();
+    switch ($method){
+       case "POST":
+          curl_setopt($curl, CURLOPT_POST, 1);
+          if ($data)
+             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+          break;
+       case "PUT":
+          curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+          if ($data)
+             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+          break;
+       case "DELETE":
+             curl_setopt($curl, CURLOPT_POST, "DELETE");
+             if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+             break;
+          default:
+          if ($data)
+             $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+       'APIKEY: 111111111111111111111',
+       'Content-Type: application/json',
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
+    curl_close($curl);
+    return $result;
+ }
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre_usr = $_POST["name"];
-    $apellido_usr = $_POST["surname"];
-    $pais_usr = $_POST["country"];
-    $email = $_POST["email"];
-    $contrasena = $_POST["pwd"];
-    $fecha = date("Y-m-d");
-    $opciones = array('cost'=>12);
 
 // Las siguientes lineas son para obtener el id del ultimo registrado
-
-    $query_cantidad = 'SELECT ID FROM USUARIO order by ID desc LIMIT 1';    
-    $query_cantidad = pg_query_params($dbconn, $query_cantidad,array());
-    $query_cantidad = pg_fetch_assoc($query_cantidad);
-    $query_cantidad = $query_cantidad["id"];
-
-    $id = $query_cantidad +1;
-
-
-    $contrasena_hasheada = password_hash($contrasena, PASSWORD_BCRYPT, $opciones);
-
-    $sql = 'INSERT INTO usuario (nombre,apellido, correo, contraseña, pais,fecha_registro, administrador, id) VALUES ($1, $2,$3,$4,$5,$6,$7,$8)';
-    if( pg_query_params($dbconn, $sql, array($nombre_usr,$apellido_usr,$email, $contrasena_hasheada,$pais_usr,$fecha,0,$id)) !== FALSE ) {
-        pg_close($dbconn);
-	echo "<script>alert('Usuario agregado con exito');document.location='../create.html'</script>";
-    } else {
-        echo "<script>alert('Usuario no se pudo agregar');document.location='../create.html'</script>";
-        pg_close($dbconn);
-    }
-
+$data_array =  array( 'id_moneda'   => $_POST['id_moneda'],
+      'valor'   => $_POST['value']);
+$update_plan = callAPI('POST', 'http://127.0.0.1:5000/api/precio', json_encode($data_array));
 }
+header("Location: /CRUD-simulacion/precio_moneda/all.html");
 ?>
+
