c108fc3c2c49338c3fc2eb6a0c1f270fdae1de73
Bastian951 <70776507+Bastian951@users.noreply.github.com>
2021-07-21 16:43:04 -0400

    Se actualiza simulacion crud moneda read.php

--- CRUD-simulacion/moneda/CRUD/read.php
+++ CRUD-simulacion/moneda/CRUD/read.php
@@ -1,30 +1,24 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT']. '/sesion/valida_sesion.php';
 
-// es esta la manera más eficiente?
-// recordar id usuario
-
-$id = preg_replace('#/admin/users/read.html\?id=#', '', $_SERVER['REQUEST_URI']);
+$id = preg_replace('#/CRUD-simulacion/moneda/read.html\?id=#', '', $_SERVER['REQUEST_URI']);
 
 if($id != '') {
-$query = 
-' SELECT usuario.nombre AS usuario
-,apellido,correo,pais.nombre AS pais
-,fecha_registro,id FROM 
-pais JOIN usuario
- ON usuario.pais=pais.cod_pais
- WHERE id = $1
- ';
 
-$result = pg_query_params($dbconn, $query, array($id));
+$json_moneda = file_get_contents('http://127.0.0.1:5000/api/moneda' );
 
-if( $result !== FALSE ) {
-    pg_close($dbconn);
-    $info_usuario = pg_fetch_assoc($result);
-} else {
-    echo "Usuario no existe";    
-    pg_close($dbconn);
-}
+$monedas= json_decode($json_moneda,true)["monedas"];
+
+$monedas = array_filter($monedas, function ($var) use ($id) {
+    return ($var['id'] == $id);
+});
+
+foreach($monedas as $i => $value){ $moneda = $value; }
+
+
+$sigla = $moneda["sigla"];
+$nombre = $moneda["nombre"];
+
+
 }else{
     pg_close($dbconn);   
     header( "Location: /");
