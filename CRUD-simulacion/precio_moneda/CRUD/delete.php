f2633887c624cead88bbf2a9b8cb0612ad1237c1
unknown <amanda.salinas@sansano.usm.cl>
2021-07-21 18:18:43 -0400

    Se agrega delete.php

--- CRUD-simulacion/precio_moneda/CRUD/delete.php
+++ CRUD-simulacion/precio_moneda/CRUD/delete.php
@@ -1,12 +1,59 @@
 <?php
-include '../../../db_config.php';
-$numbers = preg_replace('/[^0-9]/', '', $_SERVER['REQUEST_URI']);
-$sql = "DELETE FROM usuario WHERE id=$numbers";
-if(pg_query($dbconn, $sql)){
-    echo "Record was deleted successfully.";
-}
-else{
-    echo "ERROR: Could not able to execute $sql. ". mysqli_error($dbconn);
-}
-header( "Location: ../all.html");
+function callAPI($method, $url, $data){
+    $curl = curl_init();
+    switch ($method){
+       case "POST":
+          curl_setopt($curl, CURLOPT_POST, 1);
+          if ($data)
+             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+          break;
+       case "PUT":
+          curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+          if ($data)
+             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+          break;
+          case "DELETE":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+            if ($data)
+               curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+       default:
+          if ($data)
+             $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+       'APIKEY: 111111111111111111111',
+       'Content-Type: application/json',
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
+    curl_close($curl);
+    return $result;
+ }
+
+$patrones= array();
+$patrones[0]='/\/CRUD-simulacion\/precio_moneda\/CRUD\/delete.php\?id\=/';
+$patrones[1] ='/delete.php\?fecha\=/';
+
+$sustituciones=array();
+$sustituciones[0]= '/';
+$sustituciones[1]= '';
+
+
+$numbers = preg_replace($patrones, $sustituciones, $_SERVER['REQUEST_URI']);
+
+$direccion= 'http://127.0.0.1:5000/api/precio' . $numbers;
+
+$update_plan = callAPI("DELETE", $direccion,false);
+
+header( "Location: /CRUD-simulacion/precio_moneda/all.html");
 ?>
+
+
+
+
