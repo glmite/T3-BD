c87573aec9aa00078610a13c35371a0db7532395
unknown <amanda.salinas@sansano.usm.cl>
2021-07-21 18:42:30 -0400

    Se agrega read.php

--- CRUD-simulacion/precio_moneda/CRUD/read.php
+++ CRUD-simulacion/precio_moneda/CRUD/read.php
@@ -1,30 +1,37 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT']. '/sesion/valida_sesion.php';
 
-// es esta la manera más eficiente?
-// recordar id usuario
+#$id = preg_replace('#/CRUD-simulacion/pais/read.html\?id=#', '', $_SERVER['REQUEST_URI']);
 
-$id = preg_replace('#/admin/users/read.html\?id=#', '', $_SERVER['REQUEST_URI']);
+$patrones= array();
+$patrones[0]='/\/CRUD-simulacion\/precio_moneda\/read.html\?id\=/';
+$patrones[1] ='/\/read.html\?fecha\=.*/';
+
+$sustituciones=array();
+$sustituciones[0]= '';
+$sustituciones[1]= '';
+
+$id = preg_replace($patrones, $sustituciones, $_SERVER['REQUEST_URI']);
+
+#se obtiene fecha de url
+$patron= '/\/CRUD-simulacion\/precio_moneda\/read.html\?id\=[0-9]+\/read.html\?fecha=/';
+$fecha= preg_replace($patron, '', $_SERVER['REQUEST_URI']);
+$fecha= preg_replace('/%20/', ' ', $fecha);
 
 if($id != '') {
-$query = 
-' SELECT usuario.nombre AS usuario
-,apellido,correo,pais.nombre AS pais
-,fecha_registro,id FROM 
-pais JOIN usuario
- ON usuario.pais=pais.cod_pais
- WHERE id = $1
- ';
 
-$result = pg_query_params($dbconn, $query, array($id));
+$json_precio = file_get_contents('http://127.0.0.1:5000/api/precio' );
 
-if( $result !== FALSE ) {
-    pg_close($dbconn);
-    $info_usuario = pg_fetch_assoc($result);
-} else {
-    echo "Usuario no existe";    
-    pg_close($dbconn);
-}
+$precios = json_decode($json_precio,true)["precios"];
+
+$precios= array_filter($precios, function ($var) use ($id,$fecha){
+    return ($var['id_moneda']==$id and $var['fecha']== $fecha);
+});
+
+foreach($precios as $i => $value){$precio= $value;}
+
+$id_moneda= $id;
+$fecha= preg_replace($patron, '', $_SERVER['REQUEST_URI']);
+
 }else{
     pg_close($dbconn);   
     header( "Location: /");
