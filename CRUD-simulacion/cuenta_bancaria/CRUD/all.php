38d1f033ed4041a3c31dace0cd3671f517b76a76
Nilsson7000 <85040945+Nilsson7000@users.noreply.github.com>
2021-07-21 18:42:39 -0400

    Update all.php

--- CRUD-simulacion/cuenta_bancaria/CRUD/all.php
+++ CRUD-simulacion/cuenta_bancaria/CRUD/all.php
@@ -1,22 +1,30 @@
 <?php
   include '../../db_config.php';
-  $sql = "SELECT * FROM usuario";
+  $sql = "SELECT * FROM cuenta_bancaria";
   $result = pg_query_params($dbconn, $sql, array());
-  if (pg_num_rows($result) > 0)
-  {
-    while ($row = pg_fetch_assoc($result))
-    {
-      $direccion_read = '<a href="/admin/users/read.html?id=' . $row["id"] . '" class="btn btn-primary">Ver <i class="fas fa-search"></i></a>';
-      //<a href="/admin/users/read.html?id=$ontador" class="btn btn-primary">Ver <i class="fas fa-search"></i></a>
-      $direccion_edit = '<a href="/admin/users/update.html?id=' . $row["id"] . '" class="btn btn-warning">Editar <i class="fas fa-edit"></i></a>';
-      //'<a href="/admin/users/update.html?id=$contador" class="btn btn-warning">Editar <i class="fas fa-edit"></i></a>'
-      $direccion_delete = '<a href="/admin/users/CRUD/delete.php?id=' . $row["id"] . '" class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a>';
-      //'<a href="/admin/users/CRUD/delete.php?id=$contador class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a>'
-      echo '<tr>' . '<td>' . $row["id"] . '<td>' . $row["nombre"] . '<td>' . $row["apellido"] . '<td>' . $row["correo"] . '<td>' .
+
+
+$json = file_get_contents('http://127.0.0.1:5000/api/cuenta_bancaria');
+
+// Converts it into a PHP object
+$cuentas = json_decode($json,true)["cuentas"];
+
+foreach($cuentas as $i => $value){
+
+      $direccion_read = '<a href="/CRUD-simulacion/cuenta_bancaria/read.html?id=' . $value["numero_cuenta"] . '" class="btn btn-primary">Ver <i class="fas fa-search"></i></a>';
+
+      $direccion_edit = '<a href="/CRUD-simulacion/cuenta_bancaria/update.html?id=' . $value["numero_cuenta"] . '" class="btn btn-warning">Editar <i class="fas fa-edit"></i></a>';
+
+      $direccion_delete = '<a href="/CRUD-simulacion/cuenta_bancaria/CRUD/delete.php?id=' . $value["numero_cuenta"] . '" class="btn btn-danger">Borrar <i class="fas fa-trash-alt"></i></a>';
+
+      echo '<tr>' . '<td>' . $value["numero_cuenta"] . '<td>' . $value["id_usuario"] . '<td>' . $value["balance"] . '<td>' .
           $direccion_read . '<td>' .
           $direccion_edit . '<td>' .
           $direccion_delete;
-    }
-  }
+
+
+}
+
+
 /* Este archivo debe manejar la lógica de obtener los datos de todos los usuarios */
 ?>
