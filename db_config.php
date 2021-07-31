11b4727fe2bfaba49d188337208dc646d082ac37
Nilsson7000 <85040945+Nilsson7000@users.noreply.github.com>
2021-07-30 20:58:45 -0400

    se subio config.php

---
+++ db_config.php
@@ -0,0 +1,13 @@
+
+    <?php
+    // detalles de la conexion
+    $conn_string = "host=localhost port=5432 dbname= user=postgres password=";
+    // establecemos una conexion con el servidor postgresSQL
+    $dbconn = pg_connect($conn_string);
+    // Revisamos el estado de la conexion en caso de errores.
+    if (!$dbconn) {
+        echo "<body><h1>Error: No se ha podido conectar a la base de datos\n</h1></body>";
+    }
+    // Cerramos la conexion
+    // pg_close($dbconn);
+    ?>
